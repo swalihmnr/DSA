65696d1387384cd0f65a1304220622f7c41be72c
swalimnr <swalimohd048@gmail.com>
2026-05-13 05:19:08 +0530

    i completed queue and stack

--- DSA-2/stack/sample.js
+++ DSA-2/stack/sample.js
@@ -1,30 +1,43 @@
-class queue{
-    constructor(){
-        this.queue=[];
-    }
-    enqueue(data){
-        this.queue.push(data)
-
-    }
-    mid(n=this.queue.length,j=0){
-        if(n===j)return
-       
-        let temp=this.queue.shift();
-        this.mid(n,j+1);
-        if(j!==Math.floor(n/2)){
-            this.queue.push(temp)
-        }
-    }
-    desplay(){
-        console.log(this.queue)
+class Node{
+    constructor(song){
+        this.song=song;
+        this.prev=null;
+        this.next=null;
     }
 }
-const q=new queue();
-q.enqueue(10)
-q.enqueue(20)
-q.enqueue(30)
-q.enqueue(40)
-q.enqueue(50)
-q.desplay()
-q.mid();
-q.desplay()
+class MusicPlayer{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.current=null;
+    }
+    addMusic(song){
+        let newSong=new Node(song)
+        if(this.head===null){
+            this.head=newSong;
+            this.tail=newSong;
+            this.current=newSong;
+        }else{
+            this.tail.next=newSong;
+            newSong.prev=this.tail;
+            this.tail=newSong;
+        }
+    }
+    next(){
+        if(this.current.next===null){
+            return console.log('there is no song');
+        }
+        this.current=this.current.next
+        return this.current.song
+    }
+    prev(){
+        if(this.current.prev===null){
+            return console.log('there is no song');
+        }
+        this.current=this.current.prev
+        return this.current.song
+    }
+    currentSong(){
+      return   this.current.song
+    }
+}
